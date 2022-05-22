dcb8216bf63a3bedbc258e4ceb98965717580f32
sakomm <sakommuri@gmail.com>
2022-05-22 14:49:25 -0400

    full pivot - moving to a new project style

--- page.js
+++
@@ -1,8 +0,0 @@
-// check and see what state the on and off buttons are in and log them to consoel
-// every time it changes
-
-const switchToggle = document.querySelector(".onoffswitch");
-
-switchToggle.addEventListener("change", () => {
-    console.log(onoffswitch.checked);
-});
